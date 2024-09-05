e710162b8a87d525d2c4756c8f58ae46466f2cfe
G7cars <g7selfdrivecars@gmail.com>
2024-09-06 01:00:04 +0530

    a

--- app/page.js
+++ app/page.js
@@ -7,13 +7,20 @@ import Whatsapp from './components/Whatsapp'
 
 const page = () => {
   return (
-    <div className='lg:space-y-10 md:space-y-10 space-y-2'>
-      <Hero />
-      <Services />
-      <Work />
-      <Projects />
-      <Whatsapp />
+    <div className='h-screen w-screen flex justify-center items-center text-2xl font-bold'>
+
+
+      This Website Will be Available Soon
+
+
     </div>
+    //<div className='lg:space-y-10 md:space-y-10 space-y-2'>
+      //<Hero />
+      //<Services />
+      //<Work />
+      //<Projects />
+      //<Whatsapp />
+    //</div>
   )
 }
 
