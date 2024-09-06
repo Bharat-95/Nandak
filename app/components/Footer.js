09205ee793a59a38b73f916f9172e3838206bf56
G7cars <g7selfdrivecars@gmail.com>
2024-09-06 08:53:22 +0530

    a

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
+import { FaWhatsappSquare } from "react-icons/fa";
+import { ImLinkedin } from "react-icons/im";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -9,6 +13,11 @@ const Footer = () => {
           <ul className='space-y-2 mt-4'>
             <li className='hover:text-teal-900 cursor-pointer'>bharat.nanavattula@nandak.co</li>
             <li className='hover:text-teal-900 cursor-pointer'>sairam.akula@nandak.co</li>
+            <li className='flex gap-2'>
+             <Link href='https://www.linkedin.com/company/nandak-innovations-private-limited/about/?viewAsMember=true' target='_blank'><ImLinkedin className='text-blue-500 w-6 h-6 hover:opacity-80 ' /></Link>
+            <RiInstagramFill className='text-pink-500 w-6 h-6 hover:opacity-80 ' />
+            <FaFacebookSquare className='text-blue-600 w-6 h-6 hover:opacity-80 ' />
+            </li>
           </ul>
         </div>
         
