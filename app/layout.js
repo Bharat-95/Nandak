d70e17b5e51d95f0256c177e8862e221f8142f1a
G7cars <g7selfdrivecars@gmail.com>
2024-10-26 15:42:14 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -2,24 +2,37 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Nandak Innovations ",
+  title: "Nandak Innovations",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-[#DDE3FE] text-black lg:px-16 md:px-16 px-4 py-6">
+      <Head>
+        {/* Google Tag Manager */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16754470845"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16754470845');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}   
         <Footer />  
-        </body>
-     
-      
+      </body>
     </html>
   );
 }
