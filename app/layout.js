7950d88d093e481158ab5a8f4b37e81c7dd6088c
G7cars <g7selfdrivecars@gmail.com>
2024-10-26 15:46:00 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -15,18 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-[#DDE3FE] text-black lg:px-16 md:px-16 px-4 py-6">
       <Head>
-        {/* Google Tag Manager */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16754470845"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16754470845');
-            `,
-          }}
-        />
+    
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16754470845">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16754470845');
+</script>
       </Head>
       <body className={inter.className}>
         <Header />
