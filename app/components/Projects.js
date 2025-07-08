ff02300606b39dfe8e36b10500baf8df89c930c8
Bharat Kumar <bharat.nanavathula@gmail.com>
2025-07-08 10:52:18 +0530

    a

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -11,62 +11,90 @@ import Link from 'next/link'
 
 const Projects = () => {
   return (
-    <div className="lg:block hidden px-[199px]">
+    <div className="lg:block hidden px-20">
         <div className="text-[#1C923F] flex justify-center text-[30px] font-semibold">Previous Projects</div>
         <div className='text-[12px] flex justify-center py-[10px] text-center'>We focus on ergonomics and meeting you where you work. <br/>
         It's only a keystroke away.</div>
-        <div className='flex justify-center gap-[33px] pt-[44px]'>
-          <div className='w-[225px] h-[285px] overflow-hidden'>
+        <div className='flex justify-center gap-[20px] pt-[44px]'>
+          <Link href='http://kaivalyamevents.com/' target='_blank' ><div className='w-[420px] h-[285px] overflow-hidden hover:translate-x-1 hover:-translate-y-1 duration-700'>
             <Image
-            src={G7Admin}
+            src= '/Kaivalyam.png'
             alt='No Image Found'
-            className='shadow-md  rounded-lg' 
+             width={100}
+            height={100}
+            className='shadow-md  rounded-lg h-full w-full' 
             unoptimized={true}/>
           </div>
+          </Link>
 
-          <div className='w-[225px] h-[285px] overflow-hidden'>
+          <Link href='https://www.qgroupmedia.com/' target='_blank'>
+          <div className='w-[420px] h-[285px] overflow-hidden hover:translate-x-1 hover:-translate-y-1 duration-700'>
             <Image
-            src={G7carsApp}
+            src= '/Qnews.png'
             alt='No Image Found'
-            className='shadow-md  rounded-lg'
+            className='shadow-md  rounded-lg h-full w-full'
+            width={100}
+            height={100}
             unoptimized={true} />
           </div>
+          </Link>
 
-          <div className='w-[225px] h-[285px] overflow-hidden'>
+          <Link href='/https://www.obsvector.com/' target='_blank'></Link>
+
+          <div className='w-[420px] h-[285px] overflow-hidden hover:translate-x-1 hover:-translate-y-1 duration-700'>
             <Image
-            src={Hostel}
+            src='/Obsvector.png'
             alt='No Image Found'
-            className='shadow-md  rounded-lg' 
+            className='shadow-md  rounded-lg h-full w-full' 
+             width={100}
+            height={100}
             unoptimized={true}/>
           </div>
         </div>
 
-        <div className='flex gap-[31px] pt-[44px]'>
-        <div className='w-[395px] h-[285px] overflow-hidden'>
+        <div className='flex gap-[20px] pt-[40px]'>
+        
+        <Link href='https://www.kisancircle.com/' target='_blank'>
+        
+        <div className='w-[420px] h-[285px] overflow-hidden hover:translate-x-1 hover:-translate-y-1 duration-700'>
             <Image
-            src={Asquare}
+            src='/KisanCircle.png'
             alt='No Image Found'
-            className='shadow-md  rounded-lg' 
+            className='shadow-md  rounded-lg h-full w-full' 
+             width={100}
+            height={100}
             unoptimized={true}/>
           </div>
 
-          <div className='w-[395px] h-[285px] overflow-hidden'>
+          </Link>
+
+          <Link href='https://www.pmconventions.com/'>
+
+          <div className='w-[420px] h-[285px] overflow-hidden hover:translate-x-1 hover:-translate-y-1 duration-700'>
             <Image
-            src={Scoolify}
+            src='/Pm.png'
             alt='No Image Found'
-            className='shadow-md  rounded-lg '
-            unoptimized={true} />
+            className='shadow-md  rounded-lg h-full w-full' 
+             width={100}
+            height={100}
+            unoptimized={true}/>
           </div>
 
+          </Link>
 
-          <div className='w-[395px] h-[285px] overflow-hidden'>
+          <Link href='https://www.g7cars.in/' target='_blank'>
+
+
+           <div className='w-[420px] h-[285px] overflow-hidden hover:translate-x-1 hover:-translate-y-1 duration-700'>
             <Image
-            src={G7cars}
+            src='/G7cars.png'
             alt='No Image Found'
-            className='shadow-md  rounded-lg '
-            unoptimized={true} />
+            className='shadow-md  rounded-lg h-full w-full' 
+             width={100}
+            height={100}
+            unoptimized={true}/>
           </div>
-
+          </Link>
         </div>
         
     </div>
